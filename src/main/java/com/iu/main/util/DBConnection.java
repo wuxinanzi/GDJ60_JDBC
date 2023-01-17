102eeca2ec8fb767a0859e04960cd88aeb0ad7da
wuxinanzi <suu308@gmail.com>
2023-01-17 17:36:00 +0900

    TEST

---
+++ src/main/java/com/iu/main/util/DBConnection.java
@@ -0,0 +1,42 @@
+package com.iu.main.util;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DBConnection {
+	
+	public void getConnection() throws Exception {
+		System.out.println("Get Connection");
+		//1. id
+		String username="hr";
+		//2. pw
+		String password="hr";
+		//3.URL
+		String url = "jdbc:oracle:thin:@192.168.1.128:1521/XEPDB1";
+		//4.driver
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		
+		Class.forName(driver);
+		
+		Connection connection = DriverManager.getConnection(url, username, password);
+		
+		String sql ="SELECT * FROM LOCATIONS";
+		
+		PreparedStatement st = connection.prepareStatement(sql);
+		
+		ResultSet rs = st.executeQuery();
+		
+		while(rs.next()){
+			System.out.println(rs.getInt("LOCATION_ID"));
+			System.out.println(rs.getString("STREET_ADDRESS"));
+			System.out.println(rs.getString("POSTAL_CODE"));
+			System.out.println(rs.getString("CITY"));
+			System.out.println(rs.getString("STATE_PROVINCE"));
+			System.out.println(rs.getString("COUNTRY_ID"));
+		}
+	}
+
+}
